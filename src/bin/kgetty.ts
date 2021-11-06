c47f8a9198ab5075cd660dbdeeb1417812b96869
Kees van Voorthuizen <me@keesvv.nl>
2021-11-06 13:25:29 +0100

    fix(kgetty): improve prompt

--- src/bin/kgetty.ts
+++ src/bin/kgetty.ts
@@ -1,20 +1,34 @@
 import { Executable } from "../lib/exec";
 import { fprint, readline } from "../lib/io";
+import { sleep } from "../lib/thread";
 
 export class KGetty extends Executable {
+  private readonly hostname = "unknown";
+
+  private async prompt(): Promise<[string, string]> {
+    let username = "";
+    while (!username.length) {
+      await fprint(this.stdio.stdout, `${this.hostname} login: `);
+      username = await readline(this.stdio.stdin);
+    }
+
+    await fprint(this.stdio.stdout, `password for ${username}: `);
+    const password = await readline(this.stdio.stdin);
+
+    return [username, password];
+  }
+
   async main(argv: string[]): Promise<number> {
     const hostname = "unknown"; // TODO: /etc/hostname
     const release = "0.1-alpha"; // TODO: /etc/os-release
 
     await fprint(
       this.stdio.stdout,
-      `\nwebnix ${release} ${hostname} ${argv[0]}\n${hostname} login: `
+      `\nwebnix ${release} ${hostname} ${argv[0]}\n`
     );
 
-    const username = await readline(this.stdio.stdin);
-    await fprint(this.stdio.stdout, `password for ${username}: `);
-
-    const password = await readline(this.stdio.stdin);
+    await this.prompt();
+    await sleep(1500);
     await fprint(this.stdio.stdout, "Authentication failure.\n");
     return 0;
   }
