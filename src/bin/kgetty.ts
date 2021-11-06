e9534f32f0032b1765afa7080ad401b0204c6cc2
Kees van Voorthuizen <me@keesvv.nl>
2021-11-06 12:24:07 +0100

    feat(kgetty): read username & password from stdin

--- src/bin/kgetty.ts
+++ src/bin/kgetty.ts
@@ -1,14 +1,21 @@
 import { Executable } from "../lib/exec";
-import { strtob } from "../lib/strconv";
+import { fprint, readline } from "../lib/io";
 
 export class KGetty extends Executable {
   async main(argv: string[]): Promise<number> {
     const hostname = "unknown"; // TODO: /etc/hostname
     const release = "0.1-alpha"; // TODO: /etc/os-release
 
-    await this.stdio.stdout.write(
-      strtob(`\nwebnix ${release} ${hostname} ${argv[0]}\n${hostname} login: `)
+    await fprint(
+      this.stdio.stdout,
+      `\nwebnix ${release} ${hostname} ${argv[0]}\n${hostname} login: `
     );
+
+    const username = await readline(this.stdio.stdin);
+    await fprint(this.stdio.stdout, `password for ${username}: `);
+
+    const password = await readline(this.stdio.stdin);
+    await fprint(this.stdio.stdout, "Authentication failure.\n");
     return 0;
   }
 }
